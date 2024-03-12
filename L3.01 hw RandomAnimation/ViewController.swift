ae79852c2cb3b68c269170a1bc23f7d7982a53d1
ValentinAmazing <podrezva@bk.ru>
2024-03-13 02:50:52 +0700

    Initial Commit

---
+++ L3.01 hw RandomAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  L3.01 hw RandomAnimation
+//
+//  Created by Valentin on 13.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
