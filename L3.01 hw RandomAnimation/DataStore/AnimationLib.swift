529324390e16df2c4fd2b4d94703f086092d362e
ValentinAmazing <podrezva@bk.ru>
2024-03-14 00:22:21 +0700

    исправлены ошибки в структуре данных

--- L3.01 hw RandomAnimation/DataStore/AnimationLib.swift
+++ L3.01 hw RandomAnimation/DataStore/AnimationLib.swift
@@ -10,28 +10,26 @@ final class AnimaLib {
     static let shared = AnimaLib()
     
     let options = (
-        force: 1.0...5,
-        duration: 0.5...5,
-        delay: 0.0...2,
-        damping: 0.0...1,
-        velocity: 0.0...1,
-        rotate: -5.0...5,
-        x: -100.0...100,
-        y: -100.0...100,
-        scale: 0.0...5
+        force: 1.0..<5,
+        duration: 0.5..<5,
+        delay: 0.0..<2,
+        damping: 0.0..<1,
+        velocity: 0.0..<1,
+        rotate: -5.0..<5,
+        x: -100.0..<100,
+        y: -100.0..<100,
+        scale: 0.0..<5
     )
   
     //вероятно, надо подобрать, для каждой опции, оптимальные диапазоны анимации и задать их индивидуально
     let sets = [
         (
             name: "slideUP",
-            curve: "spring",
-            options: shared.options
+            curve: "spring"
         ),
         (
             name: "zoomIn",
-            curve: "easeOutQuin",
-            options: shared.options
+            curve: "easeOutQuin"
         )
     ]
     
--- L3.01 hw RandomAnimation/Models/AnimationOption.swift
+++ L3.01 hw RandomAnimation/Models/AnimationOption.swift
@@ -12,7 +12,7 @@ struct AnimationOption {
     let force: Double //1...5
     let duration: Double //0.5...5
     let delay: Double //0...2
-    
+
     let damping: Double //0...1
     let velocity: Double //0...1
     let rotate: Double //-5...5
@@ -22,21 +22,22 @@ struct AnimationOption {
     
     static func getRandomAnimation() -> AnimationOption {
         let animation = AnimaLib.shared.sets.randomElement() ?? AnimaLib.shared.sets[0]
+        let option = AnimaLib.shared.options
         
         return AnimationOption(
             name: animation.name,
             curve: animation.curve,
-            
-            force: Double.random(in: animation.options.force),
-            duration: Double.random(in: animation.options.duration),
-            delay: Double.random(in: animation.options.delay),
-            
-            damping: Double.random(in: animation.options.damping),
-            velocity: Double.random(in: animation.options.velocity),
-            rotate: Double.random(in: animation.options.rotate),
-            x: Double.random(in: animation.options.x),
-            y: Double.random(in: animation.options.y),
-            scale: Double.random(in: animation.options.scale)
+
+            force: Double.random(in: option.force),
+            duration: Double.random(in: option.duration),
+            delay: Double.random(in: option.delay),
+
+            damping: Double.random(in: option.damping),
+            velocity: Double.random(in: option.velocity),
+            rotate: Double.random(in: option.rotate),
+            x: Double.random(in: option.x),
+            y: Double.random(in: option.y),
+            scale: Double.random(in: option.scale)
         )
     }
 }
